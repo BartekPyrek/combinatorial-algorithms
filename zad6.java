b404d652abce56d3386c79cbd5cd04a563e3548e
Bartosz Pyrek <Bartosz.Pyrek@dnvgl.com>
2019-12-03 11:06:12 +0100

    Small modification in exercise number 6.

--- zad6.java
+++ zad6.java
@@ -11,11 +11,11 @@ public class zad6 {
         System.out.print("Wprowadź liczbę by wypisać jej kompozcje: ");
         wybrana_liczba = wczytajLiczbe.nextInt();
         System.out.println();
-        generujKombinacje(wybrana_liczba, "");
+        generujKompozycje(wybrana_liczba, "");
         wczytajLiczbe.close();
     }
 
-    static void generujKombinacje(int n, String tekst) {
+    static void generujKompozycje(int n, String tekst) {
         if (n == 0) {
             System.out.println("Kompozycja liczby " + wybrana_liczba + " numer " + licznik2 + ") "
                     + tekst.substring(0, tekst.length() - 1));
@@ -24,7 +24,7 @@ public class zad6 {
         }
 
         for (int i = n; i >= 1; i--) {
-            generujKombinacje(n - i, tekst + i + "+");
+            generujKompozycje(n - i, tekst + i + "+");
         }
     }
 }
